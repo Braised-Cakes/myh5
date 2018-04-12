4068d5d12bd621a152e143e0d99055b420e6abba
Braised-Cakes <1930003003@qq.com>
2018-04-12 22:16:22 +0800

    refactor: element-ui support tree-shaking

--- src/main.js
+++ src/main.js
@@ -5,28 +5,48 @@ import 'animate.css'
 import '@/css/reset.css'
 import '@/css/index.css'
 import '@/css/index.scss'
-import ElementUI from 'element-ui'
+import {
+  Pagination,
+  Scrollbar,
+  Tooltip,
+  Loading,
+  Button,
+  ColorPicker,
+  Collapse,
+  CollapseItem,
+  InputNumber,
+  Select,
+  Option,
+  MessageBox,
+  Notification,
+  Message
+} from 'element-ui';
 import '@/css/element-variables.scss'
 import '@/directive/select'
 import '@/directive/drag'
-
 import '@/iconfont/iconfont'
 import '@/iconfont/iconfont.css'
 import '@/keycode/index'
-
-// Vue.config.productionTip = false
-/* eslint-disable no-new */
 import store from '@/store'
-Vue.use(ElementUI);
-// export default new Vue({
-//     el: '#app',
-//     // router,
-//     // store,
-//     components: {
-//         App
-//     },
-//     template: '<App/>'
-// });
+// Vue.config.productionTip = false
+Vue.use(Pagination);
+Vue.use(Scrollbar);
+Vue.use(Tooltip);
+Vue.use(Button);
+Vue.use(ColorPicker);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
+Vue.use(InputNumber);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Loading.directive);
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
 
 export default new Vue({
   router: router,
